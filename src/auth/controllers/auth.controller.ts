12b4fcb45ae6211b3c698986212cc8a300e30b55
Roni Egbu <ronniiii@yahoo.com>
2025-06-18 10:36:11 +0100

    fix(auth): set cookie secure flag based on environment

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -139,7 +139,7 @@ export class AuthController {
 
     response.cookie('access_token', result.accessToken, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
       path: '/',
       maxAge: 3600000,
