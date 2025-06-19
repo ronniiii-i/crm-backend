8b2a9abf8500ab64b3236becceba7891e9a12a36
Roni Egbu <ronniiii@yahoo.com>
2025-06-19 16:32:53 +0100

    feat(swagger): integrate Swagger for API documentation and enhance cookie parsing

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser'; // <-- Add this import
+import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CRM API')
+    .setDescription('API for ClaspCRM.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // Configure CORS - ABSOLUTELY ESSENTIAL for cross-origin cookie sending
   app.enableCors({
